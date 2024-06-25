e79986fa5e610ff7e95cd80717a7204d23988b41
Eshan Nayanapriya <161910749+Eshan-Nayanapriya@users.noreply.github.com>
2024-06-25 13:06:06 +0530

    usestate

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -8,19 +8,19 @@ const Background = ({playStatus,heroCount}) => {
 
   if (playStatus){
     return(
-      <video className='background' autoPlay loop muted>
+      <video className='background fade-in' autoPlay loop muted>
         <source src={video1} type='video/mp4' />
       </video>
     )
   }
   else if (heroCount === 0){
-    return <img className='background' src={image1} alt='image1' />
+    return <img className='background fade-in' src={image1} alt='image1' />
   }
   else if (heroCount === 1){
-    return <img className='background' src={image2} alt='image2' />
+    return <img className='background fade-in' src={image2} alt='image2' />
   }
   else if (heroCount === 2){
-    return <img className='background' src={image3} alt='image3' />
+    return <img className='background fade-in' src={image3} alt='image3' />
   }
 }
 
